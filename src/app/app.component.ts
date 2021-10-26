243b26cb004564e587d8b838a10f705a21e4362c
Pierre <53476143+RoYaLSleT@users.noreply.github.com>
2021-10-26 14:08:10 +0200

    Fin Dojo POO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Player } from './shared/Player.model';
+import { Players } from './shared/Players.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularScoresDojo';
+export class AppComponent implements OnInit{
+  listeJoueur: Players
+
+  constructor() {
+    this.listeJoueur = new Players()
+  }
+  ngOnInit(): void {
+  this.listeJoueur.addPlayer(new Player("Jade", 50))
+  this.listeJoueur.addPlayer(new Player("Camille", 150))
+  this.listeJoueur.addPlayer(new Player("Vianney", -10))
+  this.listeJoueur.addPlayer(new Player("Ario", 18599))
+  console.log(this.listeJoueur.getTopThree())
+  this.listeJoueur.modifyPlayer("Ario", -18598)
+  console.log(this.listeJoueur.getTopThree())
+  }
+
+
 }
---
+++ src/app/shared/Players.model.ts
@@ -0,0 +1,32 @@
+import { Player } from './Player.model';
+
+export class Players {
+  playersList: Player[];
+  constructor() {
+    this.playersList = new Array();
+  }
+
+  addPlayer(player: Player): void {
+    this.playersList.push(player);
+  }
+
+  modifyPlayer(playerName: string, modifier: number): void {
+    // Pour chaque joueur dans le tableau des scores
+    this.playersList.forEach((element) => {
+      // mettre à jour le score actuel du joueur
+      if (element.name === playerName) {
+        element.score += Math.floor(modifier);
+      }
+    });
+  }
+
+  getTopThree(): Player[] {
+    // création d'une copie
+    const tempPlayers: Player[] = []
+    this.playersList.forEach(element=> tempPlayers.push(element))
+    // On trie par ordre decroissant sur le score des joueurs
+    tempPlayers.sort((joueur1, joueur2) => joueur2.score - joueur1.score);
+    // On coupe les 3 premiers scores (les plus haut) et on les retourne.
+    return tempPlayers.slice(0, 3);
+  }
+}
